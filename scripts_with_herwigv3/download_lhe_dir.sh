cb2b4f8c99bac71b418076914b7a2995161cfd92
Andrew Brown <ava7@live.co.uk>
2022-03-27 17:07:28 +0100

    Delete scripts_with_herwigv3 directory

--- scripts_with_herwigv3/download_lhe_dir.sh
+++
@@ -1,1 +0,0 @@
-sshpass -p "f31t03y89t[k ;jpeg k;wlm" scp -r abrown@higgs.hep.manchester.ac.uk:/higgs-data1/abrown/$process_name /home/andrew/new_install
--- scripts_with_herwigv3/plot_significance_vs_SM.sh
+++
@@ -1,6 +0,0 @@
-#2D scan of BSM
-BSM_2D_SCAN_DIR="./BSM_same_sign_muon_scattering2/ETAagainstDARK_PHOTON_MASS_scan"
-#Single scan of SM
-SM_SINGLE_RUN_DIR="./SM_same_sign_muon_scattering/"
-#Single Scan of backgrounds
-BACKGROUND1_RUN_DIR="./BACKGROUND_DIR/"
